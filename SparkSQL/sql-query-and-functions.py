92b9c622604efbce1f0f910a69b335cb28039731
Advait Joshi <138096648+adv-AI-t@users.noreply.github.com>
2024-11-24 14:37:34 +0530

    better folder structure for datasets

--- SparkSQL/sql-query-and-functions.py
+++ SparkSQL/sql-query-and-functions.py
@@ -6,7 +6,7 @@ def parseLine(line):
     fields = line.split(",")
     return Row(id = int(fields[0]), name = str(fields[1].encode("utf-8")), age = int(fields[2]), numFriends = int(fields[3]))
 
-lines = spark.sparkContext.textFile("file:///Users/advait/Documents/Courses/SparkCourse/SparkSQL/fakefriends.csv")
+lines = spark.sparkContext.textFile("file:///Users/advait/Documents/Courses/SparkCourse/SparkSQL/Datasets/fakefriends.csv")
 people = lines.map(parseLine)
 
 peopleSchema = spark.createDataFrame(people).cache()
